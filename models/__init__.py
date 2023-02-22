8ac216ccfc3c4e9c073c468d78e953a55041a750
Jenny <5704@holbertonstudents.com>
2023-02-21 20:27:27 -0800

    Updated __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-Import storage
-"""
-from models.engine import file_storage
 
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
