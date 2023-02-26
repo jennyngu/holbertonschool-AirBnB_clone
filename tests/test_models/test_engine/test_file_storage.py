601370e40ad309067d1f516ef593504e1af67dc7
Jenny <5704@holbertonstudents.com>
2023-02-26 15:12:16 -0800

    Added tests for new and all

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ Unittest for FileStorage([...])
 from models.base_model import BaseModel
 from models import storage
 from models.engine.file_storage import FileStorage
+from models.user import User
 import unittest
 import os
 
@@ -21,20 +22,35 @@ class TestFileStorage(unittest.TestCase):
         expected_str = b.__str__()
         self.assertEqual(b.__str__(), expected_str)
 
+    def test_all(self):
+        f = FileStorage()
+        all_objs = f.all()
+        self.assertIsNotNone(all_objs)
+        self.assertEqual(dict, type(all_objs))
+        self.assertIs(all_objs, f._FileStorage__objects)
+
+    def test_new(self):
+        user = User()
+        self.storage.new(user)
+        key = f"{user.__class__.__name__}.{user.id}"
+        self.assertIn(key, self.storage.all())
+
     def test_save(self):
         f = FileStorage()
         f.save()
         self.assertTrue(os.path.exists("file.json"))
 
     def setUp(self):
-        b = BaseModel()
-        b.save()
+        self.storage = FileStorage()
+        self.storage.save()
 
     def tearDown(self):
         os.remove("file.json")
 
     def test_reload(self):
-        self.storage = FileStorage()
+        user = User()
+        self.storage.new(user)
+        self.storage.save()
         self.storage.reload()
-        FileStorage().reload()
-        self.assertIsNotNone(self.storage.all())
+        key = f"{user.__class__.__name__}.{user.id}"
+        self.assertIn(key, self.storage.all())
